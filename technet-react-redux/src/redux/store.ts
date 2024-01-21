7c76876b6c02319cd249ba1fb59060ddfc42838f
Kamrul Islam Emon <10978502+wickedaf@users.noreply.github.com>
2024-01-21 19:06:45 +0600

    Challange 2 Complete, Dynamic product loading using RTK Query from MongoDB

--- technet-react-redux/src/redux/store.ts
+++ technet-react-redux/src/redux/store.ts
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
 import filterReducer from './features/filter/filterSlice';
+import { api } from './api/apiSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     filter: filterReducer,
+    [api.reducerPath]: api.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
